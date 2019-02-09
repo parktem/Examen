eed73b330b49173720c278278f70bde8f3f66d3e
parktem <alejandromayo98@gmail.com>
2019-02-09 21:19:58 +0100

    Controller Film completado

--- src/main/java/es/salesianos/controller/FilmController.java
+++ src/main/java/es/salesianos/controller/FilmController.java
@@ -3,8 +3,11 @@ package es.salesianos.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import es.salesianos.model.Film;
 import es.salesianos.service.FilmService;
 
 @Controller
@@ -13,11 +16,25 @@ public class FilmController {
 	@Autowired
 	FilmService service;
 
-	@GetMapping
+	@GetMapping(path = "/film")
 	public ModelAndView getFilms() {
 		ModelAndView model = new ModelAndView("film");
 		model.addObject("listAllFilms", service.selectAllFilm());
 		return model;
+	}
+
+	@GetMapping(path = "/deleteFilm")
+	public String deleteFilm(@RequestParam String cod) {
+		service.delete(cod);
+		return "index";
+	}
+
+	@PostMapping(path = "/insertFilm")
+	public String insertFilm(@RequestParam String title) {
+		Film film = new Film();
+		film.setTitle(title);
+		service.insert(film);
+		return "index";
 	}
 
 }
--- src/main/webapp/WEB-INF/jsp/film.jsp
+++ src/main/webapp/WEB-INF/jsp/film.jsp
@@ -14,7 +14,7 @@
 <%
 List<Film> listAllFilms = (List<Film>)request.getAttribute("selectAllFilms");
 %>
-	<form action="/film" method="post">
+	<form action="/insertFilm" method="post">
 		<span>title:</span> <input type="text" name="title"> <br />
 		<input type="submit">
 	</form>
@@ -34,7 +34,7 @@ List<Film> listAllFilms = (List<Film>)request.getAttribute("selectAllFilms");
 				<td><c:out value="${film.cod}"/> </td>
 				<td><c:out value="${film.title}"/> </td>
 				<td><c:out value="${film.codDirector}"/> </td>
-				<td><a href="/film?cod=${film.cod}">Delete</a> </td>
+				<td><a href="/deleteFilm?cod=${film.cod}">Delete</a> </td>
 	    	</tr>
 		</c:forEach>
 	</tbody>
